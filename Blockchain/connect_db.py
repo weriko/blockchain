e23091e7ddf2f0ad9b558582fe74723df0583322
santiagocanc <santiagocanchilac@gmail.com>
2022-11-27 00:58:41 -0500

    .

--- Blockchain/connect_db.py
+++ Blockchain/connect_db.py
@@ -118,7 +118,7 @@ def get_nodes():
         db.execute("SELECT * FROM nodes")
         nodes = []
         for i in db.fetchall():
-            nodes.apppend(i)
+            nodes.append(i)
 
 
         nodes.sort(key = lambda x: x[2])
