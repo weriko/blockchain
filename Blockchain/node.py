1e91ae41f49f1c16c8e3e4dd1656fe9ba88edef4
weriko <werikolgb@gmail.com>
2022-11-27 21:08:37 -0500

    ,

--- Blockchain/node.py
+++ Blockchain/node.py
@@ -121,6 +121,7 @@ class NodeAsServer(protocol.Protocol):
                 print(self.factory.node.port)
                 print(self.factory.node.ip)
                 print("Transmitting to other nodes...")
+            self.transport.loseConnection()
 
 
 # Used when node is acting as a server, receiving information from other nodes to verify, or from others to, for example, add a node to the network
@@ -245,7 +246,7 @@ class Node:
 
                         f = EchoFactory(data=json.dumps(data).encode(
                             "ascii"),  ip=n[0], port=n[1])
-                        reactor.connectTCP(n[0], n[1], f)
+                        reactor.connectTCP(n[0], n[1], f, timeout = 10)
 
                 except:
                     pass
